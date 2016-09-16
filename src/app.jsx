d255ff2478bfa1d9b28000c5fc742b0b3bb33821
jdq702 <Tomasz.Samczynski@capitalone.com>
2016-09-16 15:02:57 -0500

    images updated

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,11 @@ var React = require('react');
 
 var Carousel = require('./Carousel');
 
-var choices = ['images/1.jpg'];
+var choices = [
+  'https://i.kinja-img.com/gawker-media/image/upload/ct0qwl2ybxh1db1tymi1.jpg',
+  'https://i.kinja-img.com/gawker-media/image/upload/xpflvzatualdizw6oznl.jpg',
+  'https://i.kinja-img.com/gawker-media/image/upload/yalsuyhmda9vxmwjt5cg.jpg'
+];
 
 ReactDOM.render(
   <Carousel choices={choices}/>,
